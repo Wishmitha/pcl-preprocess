044ed5868f73afd8d154768eeaa5b029a161a764
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2019-11-26 03:37:03 +0800

    Implement converter

--- coverter.py
+++ coverter.py
@@ -1,2 +1,17 @@
-def pts_to_pcd(file):
-    return 0
+from numpy import loadtxt
+from numpy import savetxt
+
+def pts_to_pcd(file, dest=None):
+
+    filename = file.split('/')[-1].split('.')[0]
+
+    if not dest:
+        dest = ''.join(file.split('/')[:-1]) + filename + '.pcd'
+    else:
+        dest = dest + '/' + filename + '.pcd'
+
+    lines = loadtxt(file)
+    size = str(lines[:, 0].size)
+    header = "VERSION .7\nFIELDS x y z\nSIZE 4 4 4\nTYPE F F F\nCOUNT 1 1 1\nWIDTH " + size + "\nHEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\nPOINTS " + size + "\nDATA ascii"
+    pcd = lines[:, [0, 1, 2]]
+    savetxt(dest, pcd, fmt="%f", header=header, comments='')
